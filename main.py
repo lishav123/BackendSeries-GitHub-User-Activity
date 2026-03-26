77fe7cda36eeed8e5c1a8989faf20e9d70519036
lishav123 <aishwaraytiwary@gmail.com>
2026-03-26 23:49:00 +0530

    added the url and tested fetching

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import asyncio
 from httpx import AsyncClient
 from rich import print
 
-async def main():
+async def fetchGh(name):
     async with AsyncClient() as client:
-        response = await client.get("https://dummy-json.mock.beeceptor.com/posts")
-        print(f"[bold magenta]{response.json()}[/bold magenta]")
+        response = await client.get(f"https://api.github.com/users/{name}/events")
+        return response.json()
+
+async def main():
+    resp = await fetchGh("lishav123")
+    print(resp)
+    print(resp[0].keys())
+    print(len(resp))
+    print(resp[0])
 
 if __name__ == '__main__':
     asyncio.run(main())
