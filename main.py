87290fe0187314d4c94240034525823964112280
lishav123 <aishwaraytiwary@gmail.com>
2026-03-27 23:30:19 +0530

    tested with some random stuff

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import asyncio
 from httpx import AsyncClient
 from rich import print
 
+def parse(data: dict) -> str:
+    if data["type"] == "CreateEvent":
+        return f"{data["actor"]["login"]} created the repo {data["repo"]["name"]}"
+
 async def fetchGh(name):
     async with AsyncClient() as client:
         response = await client.get(f"https://api.github.com/users/{name}/events")
-        return response.json()
+        return list(map(parse, response.json()))
 
 async def main():
     resp = await fetchGh("lishav123")
     print(resp)
-    print(resp[0].keys())
-    print(len(resp))
-    print(resp[0])
 
 if __name__ == '__main__':
     asyncio.run(main())
